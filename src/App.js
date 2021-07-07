6d49c3bfb35b12c833f9a986227e8ee8913498af
happyjy <okwoyjy@gmail.com>
2021-07-07 20:21:51 +0900

    프로젝트에 router 설정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import TodoListContainer from './containers/TodoListContainer';
-import TodoFormContainer from './containers/TodoFormContainer';
-import UserListContainer from './containers/UserListContainer';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Todos from './pages/Todos';
+import Users from './pages/Users';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <UserListContainer />
-        <TodoListContainer />
-        <TodoFormContainer />
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/todos" exact component={Todos} />
+      <Route path="/users" exact component={Users} />
+    </BrowserRouter>
   );
 }
 
